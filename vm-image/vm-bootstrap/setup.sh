e6bb635e6cf34c08549a6d5fb1270de23c6e64f3
Hagen Paul Pfeifer <hagen@jauu.net>
2021-02-10 21:25:39 +0100

    update setup.sh
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- vm-image/vm-bootstrap/setup.sh
+++ vm-image/vm-bootstrap/setup.sh
@@ -6,16 +6,19 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 cp sudoers /etc/sudoers
-cp source.list /etc/apt/source.list
-apt-get --yes --force-yes update
-apt-get --yes --force-yes upgrade
-
-apt-get --yes --force-yes install mc-edit build-essential vim-nox nano llvm-11-dev llvm-11-tools
+cp sources.list /etc/apt/sources.list
+apt-get --yes update
+apt-get --yes upgrade
+apt-get --yes install aptitude mcedit build-essential vim-nox nano zsh bash
+apt-get --yes llvm-11-dev llvm-11-tools lsof gdb
+apt-get --yes install perf-tools-unstable linux-perf strace ltrace 
+apt-get --yes install binutils-bpf bpfcc-tools bpftool bpftrace libbpf-dev linux-image-amd64-dbg
 
 # we are root, now do with do unpriv operations
-su john
-cd $HOME
-mkdir src
-cd src
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-git clone https://github.com/hgn/flepa-examples.git
+su -c " \
+cd /home/john; \
+mkdir src; \
+cd src; \ 
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git; \ 
+git clone https://github.com/hgn/flepa-examples.git; \
+" john
