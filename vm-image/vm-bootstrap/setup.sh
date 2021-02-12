7da367a3c3ec7e7be0ea81f1f382868002163863
Hagen Paul Pfeifer <hagen@jauu.net>
2021-02-12 21:38:25 +0100

    vm: setup perf sysctl
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- vm-image/vm-bootstrap/setup.sh
+++ vm-image/vm-bootstrap/setup.sh
@@ -7,24 +7,31 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-cp ${BASEPATH}/sudoers /etc/sudoers
 cp ${BASEPATH}/sources.list /etc/apt/sources.list
+cp ${BASEPATH}/99-perf.conf /etc/sysctl.d/99-perf.conf
 apt-get --yes update
 apt-get --yes upgrade
 # remove some unrequired files, debug package requires some free mem
 rm -rf /var/cache/apt/archives/*
-apt-get --yes install linux-image-amd64-dbg
+apt-get --yes install linux-image-amd64-dbg sudo
 apt-get --yes install aptitude mcedit build-essential vim-nox nano zsh bash screen tmux
 apt-get --yes install llvm-11-dev llvm-11-tools lsof gdb cscope
 apt-get --yes install perf-tools-unstable linux-perf strace ltrace trace-cmd
 apt-get --yes install binutils-bpf bpfcc-tools bpftool bpftrace libbpf-dev
+# for building perf
+apt-get --yes install bison flex libdw-dev systemtap-sdt-dev libunwind-dev
+apt-get --yes install libssl-dev libslang2-dev python3-dev binutils-dev libcap-dev
+# do it now, after installation, to overwrite it in any case
+cp ${BASEPATH}/sudoers /etc/sudoers
+
+rm -rf /var/cache/apt/archives/*
 
 # we are root, now do with do unpriv operations
 su -c " \
 cd /home/john; \
 mkdir src; \
 cd src; \ 
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git; \ 
+git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git; \ 
 git clone --depth 1 https://github.com/hgn/flepa-examples.git; \
 " john
 
