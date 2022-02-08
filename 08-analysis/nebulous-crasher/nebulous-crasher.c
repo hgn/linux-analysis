affb8fa277b84518d45c79c9c16a401cbd2eb13d
Hagen Paul Pfeifer <hagen@jauu.net>
2022-02-08 20:35:24 +0100

    rename example
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- 08-analysis/nebulous-crasher/nebulous-crasher.c
+++ 08-analysis/nebulous-crasher/nebulous-crasher.c
@@ -10,13 +10,13 @@ static unsigned int nesting;
 static void left_path(void);
 static void right_path(void);
 
-static __always_inline void crasher(void)
+static always_inline void crasher(void)
 {
 	if (nesting++ > CRASH_NESTING_LVL)
 		nesting /= 0;
 }
 
-static __always_inline void pick_path(void)
+static always_inline void pick_path(void)
 {
 	int value = rand() % 2;
 	if (value)
