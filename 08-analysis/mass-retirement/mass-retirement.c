15b9fd3ff6f510b4ce75c740bc0aca6d12d90ef5
Hagen Paul Pfeifer <hagen@jauu.net>
2022-09-24 11:30:35 +0200

    code cleanup
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- 08-analysis/mass-retirement/mass-retirement.c
+++ 08-analysis/mass-retirement/mass-retirement.c
@@ -3,23 +3,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define M_REPEAT_1 \
+	do { \
+		asm("inc %r8"); \
+		asm("inc %r9"); \
+		asm("inc %r10"); \
+		asm("inc %r11"); \
+		asm("inc %r12"); \
+		asm("inc %r13"); \
+		asm("inc %r14"); \
+		asm("inc %r15"); \
+	} while (0)
+#define M_REPEAT_2 M_REPEAT_1; M_REPEAT_1
+#define M_REPEAT_4 M_REPEAT_2; M_REPEAT_2
+#define M_REPEAT_8 M_REPEAT_4; M_REPEAT_4
+#define M_REPEAT_16 M_REPEAT_8; M_REPEAT_8
+#define M_REPEAT_32 M_REPEAT_16; M_REPEAT_16
+#define M_REPEAT_64 M_REPEAT_32; M_REPEAT_32
+#define M_REPEAT_128 M_REPEAT_64; M_REPEAT_64
 
-
-int main(int ac, char **av)
+int main(void)
 {
-	//uint64_t a = ac, b = ac, c = ac, d = ac;
-	uint64_t iterations = 10000000000;
-	(void) av;
+	uint64_t iterations = 100000000;
 	while (iterations--) {
-		asm("inc %r8");
-		asm("inc %r9");
-		asm("inc %r10");
-		asm("inc %r11");
-		asm("inc %r12");
-		asm("inc %r13");
-		asm("inc %r14");
-		asm("inc %r15");
-		//a++, b++, c++, d++;
+		M_REPEAT_128;
 	}
-	//return (int)(a + b + c +d);
+	return 0;
 }
