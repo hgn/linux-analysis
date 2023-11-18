266eef32b23450b0a38f99b5335e2f9dc79cd891
Hagen Paul Pfeifer <hagen@jauu.net>
2023-11-18 22:23:25 +0100

    greedy-yielders: initial commit
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

---
+++ src/greedy-yielders/greedy-yielders.c
@@ -0,0 +1,82 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define LOOP_OUTER_MAX 1000000
+#define LOOP_INNER_MAX 100000
+
+#define EXIT_THRESHOLD 0.005
+
+
+static double xtime(void)
+{
+    struct timespec ts;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return (double)ts.tv_sec + (double)ts.tv_nsec / 1e9;
+}
+
+static void xpthread_set_selfname(const char *name) {
+        int ret;
+        ret = pthread_setname_np(pthread_self(), name);
+        if (ret != 0) {
+            printf("pthread_setname_np %d errno %s\n", ret, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+}
+
+static void yielder(void)
+{
+	unsigned i, j;
+	double before, diff;
+	volatile unsigned pad;
+
+	for (i = 0; i < LOOP_OUTER_MAX; i++) {
+		before = xtime();
+		for (j = 0; j < LOOP_INNER_MAX; j++) {
+			pad = pad | 0x23 | j * 23;
+		}
+		sched_yield();
+		diff = xtime() - before;
+		if (diff > EXIT_THRESHOLD) {
+			printf("Threshold of %lf exceeded: %lf at loop iteration %u\n",
+				EXIT_THRESHOLD, diff, i);
+			sleep(1);
+			exit(0);
+		}
+	}
+
+}
+
+static void *thread_bootstrapper(void *arg)
+{
+	(void) arg;
+
+	xpthread_set_selfname("yielder-2");
+	yielder();
+	pthread_exit(NULL);
+}
+
+int main()
+{
+	pthread_t thread;
+
+	if (pthread_create(&thread, NULL, thread_bootstrapper, (void *)NULL) != 0) {
+		perror("Error creating thread");
+		exit(EXIT_FAILURE);
+	}
+	if (pthread_detach(thread) != 0) {
+		perror("Error detaching thread");
+		exit(EXIT_FAILURE);
+	}
+
+	xpthread_set_selfname("yielder-1");
+	yielder();
+
+	return 0;
+}
