aa680e9aeea56c5bc40c9dbbe8201e7f3c798611
Hagen Paul Pfeifer <hagen@jauu.net>
2023-11-19 17:04:12 +0100

    greedy-yielders: some beautifications
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- src/greedy-yielders/greedy-yielders.c
+++ src/greedy-yielders/greedy-yielders.c
@@ -21,7 +21,9 @@ static double xtime(void)
     return (double)ts.tv_sec + (double)ts.tv_nsec / 1e9;
 }
 
-static void xpthread_set_selfname(const char *name) {
+
+static void xpthread_set_selfname(const char *name)
+{
         int ret;
         ret = pthread_setname_np(pthread_self(), name);
         if (ret != 0) {
@@ -29,6 +31,7 @@ static void xpthread_set_selfname(const char *name) {
             exit(EXIT_FAILURE);
         }
 }
+
 
 static void yielder(void)
 {
@@ -44,14 +47,17 @@ static void yielder(void)
 		sched_yield();
 		diff = xtime() - before;
 		if (diff > EXIT_THRESHOLD) {
-			printf("Threshold of %lf exceeded: %lf at loop iteration %u\n",
-				EXIT_THRESHOLD, diff, i);
+			char name[16 + 1];
+			pthread_getname_np(pthread_self(), name, sizeof(name));
+			printf("Threshold of %lf exceeded: %lf at loop iteration %u for thread \"%s\"\n",
+				EXIT_THRESHOLD, diff, i, name);
 			sleep(1);
 			exit(0);
 		}
 	}
 
 }
+
 
 static void *thread_bootstrapper(void *arg)
 {
@@ -62,9 +68,13 @@ static void *thread_bootstrapper(void *arg)
 	pthread_exit(NULL);
 }
 
-int main()
+
+int main(int ac, char **av)
 {
 	pthread_t thread;
+
+	(void)ac;
+	(void)av;
 
 	if (pthread_create(&thread, NULL, thread_bootstrapper, (void *)NULL) != 0) {
 		perror("Error creating thread");
