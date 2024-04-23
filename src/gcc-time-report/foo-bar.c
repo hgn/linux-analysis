69e236366178853ba52e6dea8b1e5544437238be
Hagen Paul Pfeifer <hagen@jauu.net>
2024-04-23 21:08:43 +0200

    matplotlib: more updates
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- src/gcc-time-report/foo-bar.c
+++ src/gcc-time-report/foo-bar.c
@@ -1,26 +1,84 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <math.h>
+#include <regex.h>
 
-static void function_three(void)
-{
-	abort();
+// Function to be executed by threads
+void *thread_function(void *arg) {
+    int *thread_id = (int *)arg;
+    
+    // Math operations
+    double square_root_result = sqrt(*thread_id);
+    double sine_result = sin(*thread_id);
+
+    printf("Thread %d is running\n", *thread_id);
+    printf("Square root of %d: %f\n", *thread_id, square_root_result);
+    printf("Sine of %d: %f\n", *thread_id, sine_result);
+
+    return NULL;
 }
 
+int main(int argc, char *argv[]) {
+    int num_threads = 2;
+    int option;
 
-static void function_two(void)
-{
-	function_three();
-}
+    // Parse command-line arguments using getopt and validate with regex
+    regex_t regex;
+    char regex_pattern[] = "^[0-9]+$";
+    if (regcomp(&regex, regex_pattern, REG_EXTENDED) != 0) {
+        fprintf(stderr, "Error compiling regex pattern\n");
+        exit(EXIT_FAILURE);
+    }
 
-static void function_one(void)
-{
-	function_two();
-}
+    while ((option = getopt(argc, argv, "n:")) != -1) {
+        switch (option) {
+            case 'n':
+                if (regexec(&regex, optarg, 0, NULL, 0) != 0) {
+                    fprintf(stderr, "Invalid argument for -n. Must be a positive integer.\n");
+                    exit(EXIT_FAILURE);
+                }
+                num_threads = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n num_threads]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
-int main(void)
-{
-	function_one();
+    // Validate the number of threads
+    if (num_threads <= 0) {
+        fprintf(stderr, "Number of threads must be greater than 0\n");
+        exit(EXIT_FAILURE);
+    }
 
-	return EXIT_SUCCESS;
+    // Create an array to store thread IDs
+    pthread_t threads[num_threads];
+    int thread_ids[num_threads];
+
+    // Spawn threads
+    for (int i = 0; i < num_threads; ++i) {
+        thread_ids[i] = i + 1; // Start thread IDs from 1
+        if (pthread_create(&threads[i], NULL, thread_function, (void *)&thread_ids[i]) != 0) {
+            fprintf(stderr, "Error creating thread %d\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Wait for threads to finish
+    for (int i = 0; i < num_threads; ++i) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            fprintf(stderr, "Error joining thread %d\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("All threads have finished\n");
+
+    // Free the regex resources
+    regfree(&regex);
+
+    return 0;
 }
